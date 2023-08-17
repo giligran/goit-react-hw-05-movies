e53c4ace6489d87f8069a49953950df9c48675e6
Rodion Kuprykov <118477139+giligran@users.noreply.github.com>
2023-08-17 08:51:18 +0000

    Update 6 files

---
+++ src/components/FilmCollectionItem/FilmCollectionItem.styled.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const CollectionItem = styled.li`
+  &:hover {
+    color: orange;
+    scale: 1.01;
+  }
+  &:focus {
+    color: orange;
+    scale: 1.01;
+  }
+`;
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,11 +1,49 @@
-import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
-import { useState, useEffect, Suspense } from 'react';
+import { NavLink, Outlet, useLocation, useParams } from 'react-router-dom';
+import { useState, useEffect, Suspense, useRef } from 'react';
 import { useFetch } from 'hooks/useFetch';
 import tmdbService from 'utils/TMDBService';
+import styled from '@emotion/styled';
+
+const Section = styled.section`
+  padding-top: 16px;
+  padding-bottom: 16px;
+  border-bottom: 1px solid var(--cornflower, #e7e9fc);
+`;
+
+const DetailsWap = styled.div`
+  display: flex;
+`;
+
+const Thumb = styled.div`
+  min-width: 300px;
+`;
+
+const MovieInfo = styled.div`
+  padding: 0 32px;
+`;
+
+const DetailsList = styled.ul`
+  display: felex;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+`;
+
+const StyledLink = styled(NavLink)`
+  font-size: 20px;
+  display: inline-block;
+  color: #2e2f42;
+  font-weight: 500;
+  line-height: 1.5;
+  letter-spacing: 0.32px;
+  &.active {
+    color: orange;
+  }
+`;
 
 function MovieDetails() {
   const location = useLocation();
-  const backLinkHref = location.state?.from ?? '/movies';
+  const backLinkHref = useRef(location.state?.from ?? '/movie');
 
   const { movieId } = useParams();
   const [imageURL, setImageURL] = useState(null);
@@ -46,33 +84,41 @@ function MovieDetails() {
   }
 
   return (
-    <div>
-      <Link to={backLinkHref} aria-label="Back to movies">
-        back
-      </Link>
-      <div>
-        <img src={imageURL} alt="" />
-        <h2>{movieDetails.original_title}</h2>
-        <p>{movieDetails.overview}</p>
-        <h3>Genres</h3>
-        <ul>
-          {movieDetails.genres.map(genre => (
-            <li key={genre.id}>{genre.name}</li>
-          ))}
-        </ul>
-        <ul>
+    <>
+      <Section>
+        <StyledLink to={backLinkHref.current} aria-label="Back to movies">
+          back
+        </StyledLink>
+        <DetailsWap>
+          <Thumb>
+            <img src={imageURL} alt="" />
+          </Thumb>
+          <MovieInfo>
+            <h2>{movieDetails.original_title}</h2>
+            <p>{movieDetails.overview}</p>
+            <h3>Genres</h3>
+            <ul>
+              {movieDetails.genres.map(genre => (
+                <li key={genre.id}>{genre.name}</li>
+              ))}
+            </ul>
+          </MovieInfo>
+        </DetailsWap>
+      </Section>
+      <Section>
+        <DetailsList>
           <li>
-            <Link to="cast">cast</Link>
+            <StyledLink to="cast">cast</StyledLink>
           </li>
           <li>
-            <Link to="reviews">reviews</Link>
+            <StyledLink to="reviews">reviews</StyledLink>
           </li>
-        </ul>
-      </div>
-      <Suspense>
-        <Outlet />
-      </Suspense>
-    </div>
+        </DetailsList>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </Section>
+    </>
   );
 }
 
